2009c6c829ee3601db5c35b5977faa81e21b3ead
Victor Guzmán Berrio <victorguzber@gmail.com>
2022-09-05 15:24:57 -0500

    -

--- src/app/interfaces/basic-response.model.ts
+++ src/app/interfaces/basic-response.model.ts
@@ -1,14 +1,5 @@
-export class BasicResponse {
-    success: boolean
-    message: string
-    data?: any
-    error?: any
-
-    constructor(success: boolean = false, message: string = "", data: any = undefined, error: any = undefined) {
-        this.success = success
-        this.message = message
-        this.data = data
-        this.error = error
-
-    }
+export interface BasicResponse {
+  status: number;
+  message: string;
+  data: any;
 }
