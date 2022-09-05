9f12a7510a0b405846db22624a6ef292d04aa9c1
Victor Guzmán Berrio <victorguzber@gmail.com>
2022-09-05 17:53:30 -0500

    modales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './components/app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './modules/app.component';
+import { SharedModule } from './modules/shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
-  providers: [],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    SharedModule,
+  ],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+
+}
--- src/app/modules/shared/components/dialog/dialog.component.ts
+++ src/app/modules/shared/components/dialog/dialog.component.ts
@@ -1,50 +1,27 @@
 import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { ModalServiceOption, ModalServiceType } from '../../../../interfaces/shared.interfaces';
+import { ModalOptions } from '../../../../interfaces/shared.interfaces';
 
 @Component({
-  selector: 'aurora-helper-dialog',
+  selector: 'app-dialog',
   templateUrl: './dialog.component.html',
   styleUrls: ['./dialog.component.scss'],
 })
-export class AuroraHelperDialogComponent {
-  icon?: string;
-  color?: string;
-
+export class DialogComponent {
   constructor(
-    public dialogRef: MatDialogRef<AuroraHelperDialogComponent>,
+    public dialogRef: MatDialogRef<DialogComponent>,
     @Inject(MAT_DIALOG_DATA)
     public data: {
-      options: ModalServiceOption;
-      type: ModalServiceType;
+      options: ModalOptions;
+      type: 'confirmation' | 'info';
     }
-  ) {
-    this.setIcon();
-  }
+  ) {}
 
-  onCancelClick(): void {
+  onCancel(): void {
     this.dialogRef.close(false);
   }
 
-  onOkClick(): void {
+  onOk(): void {
     this.dialogRef.close(true);
-  }
-
-  setIcon() {
-    this.color = this.data.type.toLowerCase();
-    switch (this.data.type) {
-      case ModalServiceType.SUCCESS:
-        this.icon = 'check_circle';
-        break;
-      case ModalServiceType.WARNING:
-        this.icon = 'error_outline';
-        break;
-      case ModalServiceType.DANGER:
-        this.icon = 'dangerous';
-        break;
-      case ModalServiceType.INFO:
-        this.icon = 'info';
-        break;
-    }
   }
 }
